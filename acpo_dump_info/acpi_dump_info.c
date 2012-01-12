23859b6aa50db8620dc25d3dcb30cb0ae39d08b5
Lekensteyn <lekensteyn@gmail.com>
2012-01-12 11:15:56 +0100

    Add new tool for dumping acpi handlers for pci addresses

---
+++ acpo_dump_info/acpi_dump_info.c
@@ -0,0 +1,65 @@
+/**
+ * Dumps information of the ACPI handles for devices
+ */
+#include <linux/pci.h>
+#include <linux/acpi.h>
+#include <linux/module.h>
+#include <asm/uaccess.h>
+#include <linux/seq_file.h>
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Dumps information of the ACPI handles for devices");
+MODULE_AUTHOR("Lekensteyn <lekensteyn@gmail.com>");
+
+#define DEVNAME "dump_info"
+
+extern struct proc_dir_entry *acpi_root_dir;
+
+static int meh_show(struct seq_file *seqfp, void *p) {
+	struct pci_dev *pdev = NULL;
+	int class = PCI_CLASS_DISPLAY_VGA << 8;
+
+	while ((pdev = pci_get_class(class, pdev)) != NULL) {
+		struct acpi_buffer buf = { ACPI_ALLOCATE_BUFFER, NULL };
+		acpi_handle handle;
+
+		handle = DEVICE_ACPI_HANDLE(&pdev->dev);
+		seq_printf(seqfp, "%s ", dev_name(&pdev->dev));
+		if (handle) {
+			acpi_get_name(handle, ACPI_FULL_PATHNAME, &buf);
+			seq_printf(seqfp, "%s\n", (char *)buf.pointer);
+		} else {
+			seq_printf(seqfp, "\n");
+		}
+	}
+	return 0;
+}
+
+static int meh_open(struct inode *inode, struct file *file) {
+	return single_open(file, meh_show, NULL);
+}
+
+static struct file_operations fops = {
+	.open    = meh_open,
+	.read    = seq_read,
+	.llseek  = seq_lseek,
+	.release = single_release,
+};
+
+static int __init meh_init(void) {
+	struct proc_dir_entry *acpi_entry;
+	acpi_entry = proc_create(DEVNAME, 0444, acpi_root_dir, &fops);
+	if (acpi_entry) {
+		printk(KERN_ERR DEVNAME ": could not create proc/acpi entry\n");
+		return -ENOMEM;
+	}
+	printk(KERN_INFO DEVNAME ": succesfully loaded in /proc/acpi/" DEVNAME);
+	return 0;
+}
+
+static void __exit meh_exit(void) {
+	remove_proc_entry(DEVNAME, acpi_root_dir);
+}
+
+module_init(meh_init);
+module_exit(meh_exit);
