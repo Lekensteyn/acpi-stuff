c8eeb5a0bd857dc9019868f4f0d06da7df2bcaba
Peter Wu <peter@lekensteyn.nl>
2016-05-23 00:51:03 +0200

    Add script for extracting DSDT/SSDT from BIOS images
    Quick script, used for analyzing Clevo BIOS images. Might works with
    others too by adjusting the included binwalk magics.

---
+++ bios-extract.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Extract DSDT and SSDT tables from a firmware image.
+# Tested with P650SE.09 from B0309.zip and others (Clevo N1xxRF, P6xxRx, ...).
+# Using binwalk v2.1.1 (with 730cdb5f02b2d43d3beb5f9573fbceda0502a4b5) and
+# custom signature magic.
+#
+# For deterministic timestamps (for easier comparison between images), use:
+#
+#   export SOURCE_DATE_EPOCH=$(date +%s);
+
+fwimage=$1
+outputdir=${2:-extracted}
+
+# DSDT/SSDT signatures for binwalk
+magicfile="$HOME/.binwalk/magic/acpi"
+if [ ! -e "$magicfile" ]; then
+    mkdir -p "${magicfile%/*}"
+    cat >"$magicfile" <<EOF
+# Match DSDT/SSDT, presumably smaller than 16 MiB (avoids false positives)
+0       regex   [SD]SDT ACPI {name:%.4s}%.4s,
+>4      ulelong x       Length: 0x%08X,{size:%d}
+>7      ubyte   !0      {invalid}
+>8      ubyte   x       Rev: 0x%02X,
+#>9      ubyte   x       Checksum: 0x%02X,
+>10     string  x       OemId: "%-6.6s",
+>16     string  x       OemTableId: "%-8.8s",
+>24     ulelong x       OemRev: 0x%08X,
+>28     string  x       CreatorId: "%-4.4s",
+>32     ulelong x       CreatorRev: 0x%08X
+EOF
+fi
+
+
+# Obtain AML (ACPI Machine Language) files from firmware image
+tmpdir=$(mktemp -d)
+trap 'rm -rf "$tmpdir"' EXIT
+binwalk -C "$tmpdir" -y lzma -y acpi -M -e -D acpi:aml "$fwimage" || exit 1
+mkdir -p "$outputdir"
+mv -vi "$tmpdir"/_*.extracted/_*.extracted/*.aml "$outputdir"/
+
+
+# Disassembly AML to ASL
+cd "$outputdir"
+echo "Invoking iasl (#1)..."
+if ! iasl -e *.aml -d *.aml; then
+    # Some possibly failed due to AE_ALREADY_EXISTS, retry with just DSDT for
+    # external symbol resolution and finally fallback to none.
+    echo "Invoking iasl (#2)..."
+    for i in *.aml; do
+        [ -e "${i%.aml}.dsl" ] || iasl -e DSDT.aml -d "$i"
+    done
+    echo "Invoking iasl (#3)..."
+    for i in *.aml; do
+        [ -e "${i%.aml}.dsl" ] || iasl -d "$i"
+    done
+fi
+# iasl calls ctime() that changes every time, override this with a fixed time.
+dt=$(LC_ALL=C date +%c ${SOURCE_DATE_EPOCH:+-d@$SOURCE_DATE_EPOCH})
+sed -r -e "s/^( \\* Disassembly of [DST0-9-]*\\.aml, ).*/\\1 $dt/" -i *.dsl
+
+
+# Get list of methods (taken from get_methods)
+echo "Dumping list of methods..."
+acpiexec -b methods *.aml |
+awk '/^$/{s=0};s==1{print $1};/^Objects of type \[Method\]/{s=1}' > methods.txt
+
+
+# Clean up for easier directory listings
+mkdir -p aml && mv *.aml aml/
